d92e60c41751671ab47a5422718d39acfd74eee1
Juniorkpabitey <juniorkpabitey123@gmail.com>
2025-05-09 21:59:48 +0000

    debugging

--- src/api.ts
+++ src/api.ts
@@ -1,18 +1,18 @@
-import { apiOptions, matchesType } from "@/types";
-const options: apiOptions = {
-  next: { revalidate: 30 },
-  headers: {
-    "X-Auth-Token": import.meta.env.API_TOKEN,
-    "Content-Type": "application/json",
-  },
-};
+// import { apiOptions, matchesType } from "@/types";
+// const options: apiOptions = {
+//   next: { revalidate: 30 },
+//   headers: {
+//     "X-Auth-Token": import.meta.env.API_TOKEN,
+//     "Content-Type": "application/json",
+//   },
+// };
 
-export const getMatchesfootball = async () => {
-  const response = await fetch('https://api.football-data.org/v4/matches', options);
-  return response.json();
-};
+// export const getMatchesfootball = async () => {
+//   const response = await fetch('https://api.football-data.org/v4/matches', options);
+//   return response.json();
+// };
 
-export const filterLeague = async (filterData: string): Promise<matchesType[]> => {
-  const allMatches = await getMatchesfootball();
-  return allMatches.matches.filter((match: matchesType) => match.competition.name === filterData);
-};
+// export const filterLeague = async (filterData: string): Promise<matchesType[]> => {
+//   const allMatches = await getMatchesfootball();
+//   return allMatches.matches.filter((match: matchesType) => match.competition.name === filterData);
+// };
