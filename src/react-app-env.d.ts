3a56327caba38b63ffdf9c6e7da8e2a745d4043f
Juniorkpabitey <juniorkpabitey123@gmail.com>
2025-05-10 19:14:15 +0000

    env string declaration

---
+++ src/react-app-env.d.ts
@@ -0,0 +1,9 @@
+declare namespace NodeJS {
+    interface ProcessEnv {
+      //REACT_APP_DEEPSEEK_API_KEY: string;
+      REACT_APP_OPENROUTER_API_KEY: string;
+      VITE_OPENROUTER_API_KEY: string;
+       
+      // Add other environment variables here as needed
+    }
+  }
